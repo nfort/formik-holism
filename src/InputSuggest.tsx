8aa9dc4f024dccf7d09a264045b4d40552d6e8fe
Малякин Кирилл <kirill.malyakin@gazprombank.ru>
2022-02-10 12:34:55 +0300

    feat: Переписанная версия InputSuggest

--- src/InputSuggest.tsx
+++ src/InputSuggest.tsx
@@ -1,72 +1,174 @@
 import React, { useCallback, useState } from "react";
-import { InputSuggest as HolismInputSuggest } from "@holism/components";
+import { InputSuggestOption } from "@holism/components";
 import { Field, FieldAttributes, FieldProps } from "formik";
-import { IProps, IPropsItem } from "@holism/components/types/new-components/InputSuggest/interfaces";
+import {
+  SuggestContainer,
+  InputContainer,
+  LabelStyle,
+  ErrorMessageStyle,
+  NoOptionStyle,
+} from "@holism/components/esm/new-components/InputSuggest/style";
+import SuggestInputField from "@holism/components/esm/new-components/InputSuggest/components/Input";
+import LoadingIndicator from "@holism/components/esm/new-components/InputSuggest/components/LoadingIndicator";
 import { RESET_BUTTON_ID } from "./ResetButton";
 import { SUBMIT_BUTTON_ID } from "./SubmitButton";
 import debounce from "lodash.debounce";
+import Autosuggest from "@nfort/react-autosuggest";
+import { GetSuggestionValue, RenderSuggestion } from "@types/react-autosuggest";
+import { IPropsItem, IProps } from "@holism/components/types/new-components/InputSuggest/interfaces";
 
-type RequieredProps = "value" | "dimension" | "options" | "onSuggestionsFetchRequested";
-type PartialProps = Omit<Partial<Pick<IProps, RequieredProps>>, "onSuggestionsFetchRequested">;
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const defaultGetSuggestionValue = (suggestion) => suggestion.value;
+
+// Use your imagination to render suggestions.
+const defaultRenderSuggestion = (suggestion) => (
+  <InputSuggestOption value={suggestion.value} label={suggestion.label} />
+);
+
+type RequieredProps = "value" | "dimension" | "options";
+type PartialProps = Partial<Pick<IProps, RequieredProps>>;
 export function InputSuggest({
   dimension = "small",
   name,
   onSuggestionsFetchRequested,
+  getSuggestionValue = defaultGetSuggestionValue,
+  renderSuggestion = defaultRenderSuggestion,
+  debounceTime = 1000,
+  onFocus,
   onChange,
+  onBlur,
   validate,
+  placeholder,
+  label,
+  noOptionsMessage,
   ...restProps
 }: PartialProps &
   Omit<IProps, RequieredProps> &
-  Partial<Pick<FieldAttributes<any>, "validate" | "name">> & {
+  Partial<Pick<FieldAttributes<any>, "validate">> &
+  Pick<FieldAttributes<any>, "name"> & {
     onSuggestionsFetchRequested: (value: string) => Promise<IPropsItem[]>;
+    renderSuggestion?: RenderSuggestion<IPropsItem>;
+    getSuggestionValue?: GetSuggestionValue<IPropsItem>;
+    debounceTime?: number;
   }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [options, setOptions] = useState<IPropsItem[]>([]);
+  const [isFocused, setFocusedFlag] = useState(false);
+  const [suggestions, setSuggestions] = useState<IPropsItem[]>([]);
   const _onSuggestionsFetchRequested = useCallback(
-    debounce((value: string) => {
+    debounce(({ value }) => {
       if (!value) {
         return;
       }
       setIsLoading(true);
       onSuggestionsFetchRequested(value)
-        .then(setOptions)
+        .then(setSuggestions)
         .finally(() => setIsLoading(false));
-    }, 1000),
+    }, debounceTime),
     []
   );
+  const _onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
   return (
     <Field name={name} validate={validate}>
       {({ form, meta, field }: FieldProps) => {
         return (
-          <HolismInputSuggest
+          <SuggestContainer
             dimension={dimension}
-            name={field.name}
-            isLoading={isLoading}
-            noOptionsMessage="Не данных"
-            onSuggestionsFetchRequested={_onSuggestionsFetchRequested}
-            value={field.value}
-            onChange={(value) => {
-              form.setFieldValue(field.name, value);
-              form.setFieldTouched(field.name, true, false);
-              onChange && onChange(value);
-            }}
-            onBlur={(event) => {
-              if (event.relatedTarget instanceof Element) {
-                /*
-                  Skip validation onBlur when reset / submit button is clicked or
-                  It will block reset / submit button onClick event
-                */
-                if (event.relatedTarget.id === RESET_BUTTON_ID || event.relatedTarget.id === SUBMIT_BUTTON_ID) {
-                  return;
-                }
-              }
-              form.handleBlur(field.name)(event);
-            }}
-            options={options}
             isError={meta.touched && !!meta.error}
-            errorMessage={meta.touched ? meta.error : undefined}
-            {...restProps}
-          />
+            isDisabled={restProps.isDisabled}
+            width={undefined}
+            hasValue={!!field.value}
+            className={restProps.className}
+          >
+            <InputContainer>
+              {label && dimension === "small" && (
+                <LabelStyle
+                  data-element="inputSuggest-label"
+                  htmlFor={name}
+                  hasValue={!!field.value}
+                  isError={meta.touched && !!meta.error}
+                >
+                  {label}
+                </LabelStyle>
+              )}
+              <Autosuggest
+                suggestions={suggestions}
+                alwaysRenderSuggestions={true}
+                getSuggestionValue={getSuggestionValue}
+                renderSuggestion={renderSuggestion}
+                inputProps={{
+                  name,
+                  value: field.value,
+                  onFocus: (event) => {
+                    setFocusedFlag(true);
+                    onFocus && onFocus(event, field.value);
+                  },
+                  onChange: (event, { newValue }) => {
+                    form.setFieldValue(field.name, newValue);
+                    form.setFieldTouched(field.name, true, false);
+                    onChange && onChange(event, newValue);
+                  },
+                  onBlur: (event) => {
+                    if (event.relatedTarget instanceof Element) {
+                      /*
+                        Skip validation onBlur when reset / submit button is clicked or
+                        It will block reset / submit button onClick event
+                      */
+                      if (event.relatedTarget.id === RESET_BUTTON_ID || event.relatedTarget.id === SUBMIT_BUTTON_ID) {
+                        return;
+                      }
+                    }
+                    form.handleBlur(field.name)(event);
+                    setFocusedFlag(false);
+                    onBlur && onBlur(event, field.value);
+                  },
+                  dimension,
+                }}
+                onSuggestionsFetchRequested={_onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={_onSuggestionsClearRequested}
+                highlightFirstSuggestion={true}
+                renderInputComponent={(inputProps: any) => {
+                  const { innerRef, ...others } = inputProps;
+                  return (
+                    <SuggestInputField
+                      styledProps={{
+                        dimension,
+                        isDisabled: restProps.isDisabled,
+                        isSuggestionsListOpened: false,
+                        isError: meta.touched && !!meta.error,
+                        isFocus: isFocused,
+                        hasValue: !!field.value,
+                        value: field.value || "",
+                      }}
+                      dimension={dimension}
+                      isFocus={isFocused}
+                      placeholder={placeholder}
+                      inputProps={{
+                        ...others,
+                        ref: innerRef,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </InputContainer>
+            {isLoading && !suggestions.length && <LoadingIndicator />}
+            {isFocused && !isLoading && !suggestions.length && noOptionsMessage && (
+              <NoOptionStyle
+                data-element="inputSuggest-noOption"
+                isError={meta.touched && !!meta.error}
+                dimension={dimension}
+              >
+                {noOptionsMessage}
+              </NoOptionStyle>
+            )}
+            {meta.touched && meta.error && (
+              <ErrorMessageStyle data-element="inputSuggest-error">{meta.error}</ErrorMessageStyle>
+            )}
+          </SuggestContainer>
         );
       }}
     </Field>
--- src/KitchenSink.tsx
+++ src/KitchenSink.tsx
@@ -7,11 +7,15 @@ import { SubmitButton } from "../src/SubmitButton";
 import { ResetButton } from "../src/ResetButton";
 import { Checkbox } from "../src/Checkbox";
 import { Select } from "../src/Select";
+import { DefaultTheme, ThemeProvider } from "@holism/components";
 
 function fetchSuggestion(address: string): Promise<any> {
   console.log("fired");
   return new Promise((resolve) => {
-    setTimeout(() => resolve([{ label: "Москва, ул Пушкина, Дом Колотушкина", value: address }]), 2000);
+    setTimeout(
+      () => resolve([{ label: "Москва, ул Пушкина, Дом Колотушкина", value: "Москва, ул Пушкина, Дом Колотушкина" }]),
+      2000
+    );
   });
 }
 
@@ -39,40 +43,51 @@ const validationSchema = object({
   date: string().required(),
 });
 
+const autocompleteOptions = [
+  { label: "москва", value: "1" },
+  { label: "париж", value: "2" },
+  { label: "прага", value: "3" },
+  { label: "краков", value: "4" },
+  { label: "иваново", value: "5" },
+  { label: "нижний новгород", value: "6" },
+];
+
 export function KitchenSink() {
   return (
-    <Formik
-      initialValues={{
-        firstName: "",
-        address: "",
-        isAgree: false,
-        country: "russia",
-        date: "2022-02-08",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={(values, formikHelpers) => {
-        alert(JSON.stringify(values, null, 2));
-        formikHelpers.setSubmitting(false);
-      }}
-    >
-      <Form>
-        <Input name="firstName" placeholder="Имя" />
-        <InputSuggest name="address" onSuggestionsFetchRequested={fetchSuggestion} />
-        <Checkbox name="isAgree" label="Согласен с правилами" />
-        <Select
-          name="country"
-          placeholder="Страна"
-          withoutOptionMessage="Ничего не найден"
-          options={[
-            { label: "Россия", value: "russia" },
-            { label: "США", value: "usa" },
-          ]}
-        />
-        <Input name="date" type="date" min="2022-02-06" max="2022-02-12" />
-        <SubmitButton>Отправить</SubmitButton>
-        <ResetButton>Сбросить</ResetButton>
-        <FormikDebug />
-      </Form>
-    </Formik>
+    <ThemeProvider theme={DefaultTheme}>
+      <Formik
+        initialValues={{
+          firstName: "",
+          address: "",
+          isAgree: false,
+          country: "russia",
+          date: "2022-02-08",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, formikHelpers) => {
+          alert(JSON.stringify(values, null, 2));
+          formikHelpers.setSubmitting(false);
+        }}
+      >
+        <Form>
+          <Input name="firstName" placeholder="Имя" />
+          <Checkbox name="isAgree" label="Согласен с правилами" />
+          <Select
+            name="country"
+            placeholder="Страна"
+            withoutOptionMessage="Ничего не найден"
+            options={[
+              { label: "Россия", value: "russia" },
+              { label: "США", value: "usa" },
+            ]}
+          />
+          <Input name="date" type="date" min="2022-02-06" max="2022-02-12" />
+          <InputSuggest name="address" placeholder="Адрес" onSuggestionsFetchRequested={fetchSuggestion} />
+          <SubmitButton>Отправить</SubmitButton>
+          <ResetButton>Сбросить</ResetButton>
+          <FormikDebug />
+        </Form>
+      </Formik>
+    </ThemeProvider>
   );
 }
